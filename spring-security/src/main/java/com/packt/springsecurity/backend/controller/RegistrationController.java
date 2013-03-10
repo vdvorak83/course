119dbbc36847e9d7bae35da08741772a23a0df77
eugenp <hanriseldon@gmail.com>
2013-03-11 01:34:01 +0200

    small cleanup

--- spring-security/src/main/java/com/packt/springsecurity/backend/controller/RegistrationController.java
+++ spring-security/src/main/java/com/packt/springsecurity/backend/controller/RegistrationController.java
@@ -10,13 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Sets;
 import com.packt.springsecurity.backend.dto.RegistrationDto;
 import com.packt.springsecurity.backend.persistence.model.Authority;
 import com.packt.springsecurity.backend.persistence.model.User;
 import com.packt.springsecurity.backend.persistence.service.IAuthorityService;
 import com.packt.springsecurity.backend.persistence.service.IUserService;
-import com.packt.springsecurity.backend.persistence.setup.SecurityConstants;
+import com.packt.springsecurity.backend.persistence.setup.SecurityConstants.Authorities;
 
 @Controller
 public class RegistrationController {
@@ -35,15 +36,14 @@ public class RegistrationController {
     @RequestMapping(value = "registration", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public void registerUser(@RequestBody final RegistrationDto registration) {
-        final Set<String> authorityNamesForUser = SecurityConstants.getNamesOfAuthoritiesForUser();
-        final Set<Authority> authorityEntitiesForUser = retrieveAuthorityEntitiesForUser(authorityNamesForUser);
+        final Set<Authority> authorityEntitiesForUser = retrieveAuthorityEntitiesForUser(ImmutableList.of(Authorities.USER));
 
         userService.create(new User(registration.getUsername(), registration.getPassword(), authorityEntitiesForUser));
     }
 
     // util
 
-    private final Set<Authority> retrieveAuthorityEntitiesForUser(final Set<String> userAuthorityNames) {
+    private final Set<Authority> retrieveAuthorityEntitiesForUser(final Iterable<String> userAuthorityNames) {
         final Set<Authority> userAuthorities = Sets.newHashSet();
         for (final String authorityName : userAuthorityNames) {
             final Authority authByName = authorityService.findByName(authorityName);
