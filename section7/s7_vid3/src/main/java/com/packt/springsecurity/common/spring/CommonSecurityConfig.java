3d057d8fd386686f78b3c87419637d37fa39ca2f
eugenp <hanriseldon@gmail.com>
2013-05-20 17:38:55 +0300

    security work

---
+++ section7/s7_vid3/src/main/java/com/packt/springsecurity/common/spring/CommonSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.packt.springsecurity.common.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource({ "classpath*:webSecurityConfig.xml", "classpath*:beans-security-acl.xml" })
+// @ImportResource({ "classpath*:webSecurityConfig.xml" })
+public class CommonSecurityConfig {
+
+    public CommonSecurityConfig() {
+        super();
+    }
+
+}
